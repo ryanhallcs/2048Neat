b6937ad4e73b8e6cb4e16bf80f61b6106bf4e0ca
Ryan Hall <ryanhallcs@gmail.com>
2014-04-22 00:03:58 -0500

    adding sharpneat and adding intial game handlers

---
+++ Neat.2048/Sharp2048.Tests/GameStateHandlerTests.cs
@@ -0,0 +1,125 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sharp2048;
+
+namespace Sharp2048.Tests
+{
+    [TestFixture]
+    public class GameStateHandlerTests
+    {
+        [Test]
+        public void MoveLeftSuccessNoMerge()
+        {
+            int size = 4;
+
+            var sut = new GameStateHandler();
+            var gameState = new GameState(size);
+            gameState.Set(0, 1, 1);
+            sut.MoveLeft(gameState);
+
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    if (!(i == 0 && j == 0))
+                        Assert.That(gameState.Get(i, j), Is.EqualTo(0));
+            Assert.That(gameState.Get(0, 0), Is.EqualTo(1));
+
+            gameState = new GameState(size);
+            gameState.Set(0, 2, 1);
+            sut.MoveLeft(gameState);
+
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    if (!(i == 0 && j == 0))
+                        Assert.That(gameState.Get(i, j), Is.EqualTo(0));
+            Assert.That(gameState.Get(0, 0), Is.EqualTo(1));
+
+            gameState = new GameState(size);
+            gameState.Set(0, 3, 1);
+            sut.MoveLeft(gameState);
+
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    if (!(i == 0 && j == 0))
+                        Assert.That(gameState.Get(i, j), Is.EqualTo(0));
+            Assert.That(gameState.Get(0, 0), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void MoveRightSuccessNoMerge()
+        {
+            int size = 4;
+
+            var sut = new GameStateHandler();
+            var gameState = new GameState(size);
+            gameState.Set(0, 1, 1);
+            sut.MoveRight(gameState);
+
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    if (!(i == 0 && j == size - 1))
+                        Assert.That(gameState.Get(i, j), Is.EqualTo(0));
+            Assert.That(gameState.Get(0, size - 1), Is.EqualTo(1));
+
+            gameState = new GameState(size);
+            gameState.Set(0, 2, 1);
+            sut.MoveRight(gameState);
+
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    if (!(i == 0 && j == size - 1))
+                        Assert.That(gameState.Get(i, j), Is.EqualTo(0));
+            Assert.That(gameState.Get(0, size - 1), Is.EqualTo(1));
+
+            gameState = new GameState(size);
+            gameState.Set(0, 0, 1);
+            sut.MoveRight(gameState);
+
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    if (!(i == 0 && j == size - 1))
+                        Assert.That(gameState.Get(i, j), Is.EqualTo(0));
+            Assert.That(gameState.Get(0, size - 1), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void MoveDownSuccessNoMerge()
+        {
+            int size = 4;
+
+            var sut = new GameStateHandler();
+            var gameState = new GameState(size);
+            gameState.Set(0, 0, 1);
+            sut.MoveDown(gameState);
+
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    if (!(i == size - 1 && j == 0))
+                        Assert.That(gameState.Get(i, j), Is.EqualTo(0));
+            Assert.That(gameState.Get(size - 1, 0), Is.EqualTo(1));
+
+            //gameState = new GameState(size);
+            //gameState.Set(0, 2, 1);
+            //sut.MoveRight(gameState);
+
+            //for (int i = 0; i < size; i++)
+            //    for (int j = 0; j < size; j++)
+            //        if (!(i == 0 && j == size - 1))
+            //            Assert.That(gameState.Get(i, j), Is.EqualTo(0));
+            //Assert.That(gameState.Get(0, size - 1), Is.EqualTo(1));
+
+            //gameState = new GameState(size);
+            //gameState.Set(0, 0, 1);
+            //sut.MoveRight(gameState);
+
+            //for (int i = 0; i < size; i++)
+            //    for (int j = 0; j < size; j++)
+            //        if (!(i == 0 && j == size - 1))
+            //            Assert.That(gameState.Get(i, j), Is.EqualTo(0));
+            //Assert.That(gameState.Get(0, size - 1), Is.EqualTo(1));
+        }
+    }
+}
---
+++ Neat.2048/Sharp2048/GameState.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharp2048
+{
+    public interface IGameState
+    {
+        int Size { get; }
+        void Reset();
+        void Set(int row, int col, int val);
+        int Get(int row, int col);
+
+        GameArray GetRow(int rowIdx);
+        GameArray GetCol(int colIdx);
+        void Set(GameArray array, int idx);
+
+        bool IsGameOver();
+    }
+    public class GameState : IGameState
+    {
+        private int[,] _internalArray;
+        private int _size;
+
+        public GameState(int size)
+        {
+            _size = size;
+            Reset();
+        }
+
+        public void Reset()
+        {
+            _internalArray = new int[_size, _size];
+        }
+
+        public void Set(int row, int col, int val)
+        {
+            _internalArray[row, col] = val;
+        }
+
+        public int Get(int row, int col)
+        {
+            return _internalArray[row, col];
+        }
+
+        public int Size
+        {
+            get { return _size; }
+        }
+
+        public GameArray GetRow(int rowIdx)
+        {
+            var result = new GameArray();
+            result.Values = new int[_size];
+            for (int i = 0; i < _size; i++)
+                result.Values[i] = _internalArray[rowIdx, i];
+            result.ArrayType = GameArrayType.Row;
+            return result;
+        }
+
+        public GameArray GetCol(int colIdx)
+        {
+            var result = new GameArray();
+            result.Values = new int[_size];
+            for (int i = 0; i < _size; i++)
+                result.Values[i] = _internalArray[i, colIdx];
+            result.ArrayType = GameArrayType.Column;
+            return result;
+        }
+
+        public void Set(GameArray array, int idx)
+        {
+            switch (array.ArrayType)
+            {
+                case GameArrayType.Column:
+                    for (int i = 0; i < _size; i++)
+                    {
+                        _internalArray[i, idx] = array.Values[i];
+                    }
+                    break;
+                case GameArrayType.Row:
+                    for (int i = 0; i < _size; i++)
+                    {
+                        _internalArray[idx, i] = array.Values[i];
+                    }
+                    break;
+                default:
+                    throw new ArgumentException("Unhandled array type" + array.ArrayType, "array");
+            }
+        }
+
+
+        public bool IsGameOver()
+        {
+            foreach (var i in _internalArray)
+            {
+                if (i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public class GameArray
+    {
+        public GameArrayType ArrayType { get; set; }
+        public int[] Values { get; set; }
+    }
+
+    public enum GameArrayType
+    {
+        Row,
+        Column
+    }
+}
