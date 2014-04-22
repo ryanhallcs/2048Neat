3069899e30b278072b947fb52a5f0be3f6677315
Ryan Hall <ryanhallcs@gmail.com>
2014-04-22 01:53:45 -0500

    fixing unit tests to understand that random tiles will show up

--- Neat.2048/Sharp2048.Tests/GameStateHandlerTests.cs
+++ Neat.2048/Sharp2048.Tests/GameStateHandlerTests.cs
@@ -21,30 +21,18 @@ namespace Sharp2048.Tests
             gameState.Set(0, 1, 1);
             sut.MoveLeft(gameState);
 
-            for (int i = 0; i < size; i++)
-                for (int j = 0; j < size; j++)
-                    if (!(i == 0 && j == 0))
-                        Assert.That(gameState.Get(i, j), Is.EqualTo(0));
             Assert.That(gameState.Get(0, 0), Is.EqualTo(1));
 
             gameState = new GameState(size);
             gameState.Set(0, 2, 1);
             sut.MoveLeft(gameState);
 
-            for (int i = 0; i < size; i++)
-                for (int j = 0; j < size; j++)
-                    if (!(i == 0 && j == 0))
-                        Assert.That(gameState.Get(i, j), Is.EqualTo(0));
             Assert.That(gameState.Get(0, 0), Is.EqualTo(1));
 
             gameState = new GameState(size);
             gameState.Set(0, 3, 1);
             sut.MoveLeft(gameState);
 
-            for (int i = 0; i < size; i++)
-                for (int j = 0; j < size; j++)
-                    if (!(i == 0 && j == 0))
-                        Assert.That(gameState.Get(i, j), Is.EqualTo(0));
             Assert.That(gameState.Get(0, 0), Is.EqualTo(1));
         }
 
@@ -58,30 +46,18 @@ namespace Sharp2048.Tests
             gameState.Set(0, 1, 1);
             sut.MoveRight(gameState);
 
-            for (int i = 0; i < size; i++)
-                for (int j = 0; j < size; j++)
-                    if (!(i == 0 && j == size - 1))
-                        Assert.That(gameState.Get(i, j), Is.EqualTo(0));
             Assert.That(gameState.Get(0, size - 1), Is.EqualTo(1));
 
             gameState = new GameState(size);
             gameState.Set(0, 2, 1);
             sut.MoveRight(gameState);
 
-            for (int i = 0; i < size; i++)
-                for (int j = 0; j < size; j++)
-                    if (!(i == 0 && j == size - 1))
-                        Assert.That(gameState.Get(i, j), Is.EqualTo(0));
             Assert.That(gameState.Get(0, size - 1), Is.EqualTo(1));
 
             gameState = new GameState(size);
             gameState.Set(0, 0, 1);
             sut.MoveRight(gameState);
 
-            for (int i = 0; i < size; i++)
-                for (int j = 0; j < size; j++)
-                    if (!(i == 0 && j == size - 1))
-                        Assert.That(gameState.Get(i, j), Is.EqualTo(0));
             Assert.That(gameState.Get(0, size - 1), Is.EqualTo(1));
         }
 
@@ -95,30 +71,18 @@ namespace Sharp2048.Tests
             gameState.Set(0, 0, 1);
             sut.MoveDown(gameState);
 
-            for (int i = 0; i < size; i++)
-                for (int j = 0; j < size; j++)
-                    if (!(i == size - 1 && j == 0))
-                        Assert.That(gameState.Get(i, j), Is.EqualTo(0));
             Assert.That(gameState.Get(size - 1, 0), Is.EqualTo(1));
 
             gameState = new GameState(size);
             gameState.Set(1, 0, 1);
             sut.MoveDown(gameState);
 
-            for (int i = 0; i < size; i++)
-                for (int j = 0; j < size; j++)
-                    if (!(i == size - 1 && j == 0))
-                        Assert.That(gameState.Get(i, j), Is.EqualTo(0));
             Assert.That(gameState.Get(size - 1, 0), Is.EqualTo(1));
 
             gameState = new GameState(size);
             gameState.Set(2, 0, 1);
             sut.MoveDown(gameState);
 
-            for (int i = 0; i < size; i++)
-                for (int j = 0; j < size; j++)
-                    if (!(i == size - 1 && j == 0))
-                        Assert.That(gameState.Get(i, j), Is.EqualTo(0));
             Assert.That(gameState.Get(size - 1, 0), Is.EqualTo(1));
         }
 
